283d89eb1259be59828a9f2996dd9c721f59b6f4
stoodkev <quentin.correa@gmail.com>
2021-03-31 19:58:02 +0800

    finalize

--- src/containers/footer.js
+++ src/containers/footer.js
@@ -31,41 +31,61 @@ const rightContainer = {
 
 // Create footer
 const Footer = () => (
-  <div className="footer" style={style}>
-    <div style={container}>
-      <a href="https://hive.io" target="_blank" rel="noreferrer">
-        <img
-          src="img/footer/powered_hive.png"
-          alt="Powered by Hive"
-          style={{ height: width > 600 ? 60 : 40 }}
-        />
-      </a>
+  <div>
+    <div className="footer" style={style}>
+      <div style={container}>
+        <a
+          href="https://hive.io"
+          target="_blank"
+          rel="noreferrer"
+          style={{ textDecoration: "none" }}
+        >
+          <img
+            src="img/footer/powered_hive.png"
+            alt="Powered by Hive"
+            style={{ height: width > 600 ? 60 : 40 }}
+          />
+        </a>
+      </div>
+      <div style={rightContainer}>
+        <a
+          href="https://github.com/stoodkev/hive-keychain-mobile"
+          target="_blank"
+          style={{ textDecoration: "none" }}
+          rel="noreferrer"
+        >
+          <span style={{ marginRight: "10vw" }}>
+            <img
+              src="img/footer/github.svg"
+              alt="Github"
+              style={{ verticalAlign: "middle" }}
+            />
+            <span style={styleText}>Visit our Github</span>
+          </span>
+        </a>
+        <a href="https://peakd.com/@keychain" target="_blank" rel="noreferrer">
+          <span>
+            <img
+              src="img/footer/see_on_hive.svg"
+              alt="Hive"
+              style={{ verticalAlign: "middle" }}
+            />
+            <span style={styleText}>Follow us on Hive</span>
+          </span>
+        </a>
+      </div>
     </div>
-    <div style={rightContainer}>
-      <a
-        href="https://github.com/stoodkev/hive-keychain-mobile"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <span style={{ marginRight: "10vw" }}>
-          <img
-            src="img/footer/github.svg"
-            alt="Github"
-            style={{ verticalAlign: "middle" }}
-          />
-          <span style={styleText}>Visit our Github</span>
-        </span>
-      </a>
-      <a href="https://peakd.com/@keychain" target="_blank" rel="noreferrer">
-        <span>
-          <img
-            src="img/footer/see_on_hive.svg"
-            alt="Hive"
-            style={{ verticalAlign: "middle" }}
-          />
-          <span style={styleText}>Follow us on Hive</span>
-        </span>
-      </a>
+    <div
+      style={{
+        width: "100%",
+        background: "#450611 0% 0% no-repeat padding-box",
+        color: "white",
+        textAlign: "center",
+        fontSize: 12,
+        paddingBottom: 5,
+      }}
+    >
+      ©2021 Hive Keychain Ltd
     </div>
   </div>
 );
