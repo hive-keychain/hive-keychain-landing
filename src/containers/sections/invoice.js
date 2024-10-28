8fc9aa67468b1b40ef94f110d52376dfc13989b3
stoodkev <quentin.correa@gmail.com>
2024-10-28 15:55:02 +0800

    keychain store invoice

---
+++ src/containers/sections/invoice.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from "react";
+
+const Invoice = () => {
+  const to = window.location.href.replace(
+    //   "http://localhost:3000/#invoice/",
+    "https://hive-keychain.com/#invoice/",
+    "hive://sign/op/"
+  );
+
+  useEffect(() => {
+    window.location.href = to;
+  }, [to]);
+
+  return (
+    <div style={{ marginVertical: "60px" }}>
+      <h1>Keychain Store Invoice</h1>
+      <p>
+        If the App you are using didn't redirect you automatically to Keychain
+        App, select "Open on Browser" among your app options, or copy paste this
+        link in your usual browser.
+        <br />
+        <br />
+        <a href={to}>Invoice link</a>
+      </p>
+    </div>
+  );
+};
+
+export default Invoice;
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const { DefinePlugin } = require("webpack");
 
